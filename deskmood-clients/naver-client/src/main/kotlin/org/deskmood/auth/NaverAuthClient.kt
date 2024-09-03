39733f451f4cc340ac56b00f1b9c2adb34707a6d
wwan13 <wwan13@naver.com>
2024-09-03 15:13:03 +0900

    feat : naver 인증 토큰 발급 API 예외 처리

--- deskmood-clients/naver-client/src/main/kotlin/org/deskmood/auth/NaverAuthClient.kt
+++ deskmood-clients/naver-client/src/main/kotlin/org/deskmood/auth/NaverAuthClient.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component
 @Component
 class NaverAuthClient(
     private val naverAuthApi: NaverAuthApi,
+    private val naverAuthErrorHandler: NaverAuthErrorHandler,
     private val naverProperties: NaverProperties
 ) {
 
@@ -19,6 +20,9 @@ class NaverAuthClient(
             state = state,
             code = code
         )
+        if (response.hasError) {
+            naverAuthErrorHandler.handle(response)
+        }
         return response.toNaverAuthToken()
     }
 }
