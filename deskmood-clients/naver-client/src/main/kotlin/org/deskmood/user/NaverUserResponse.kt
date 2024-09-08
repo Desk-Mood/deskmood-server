10362d968b0f414a3f9eb23b25dc4164bb63a682
wwan13 <wwan13@naver.com>
2024-09-08 13:33:53 +0900

    feat : naver user 추가 정보 조회

--- deskmood-clients/naver-client/src/main/kotlin/org/deskmood/user/NaverUserResponse.kt
+++ deskmood-clients/naver-client/src/main/kotlin/org/deskmood/user/NaverUserResponse.kt
@@ -1,15 +1,36 @@
 package org.deskmood.user
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.deskmood.datetime.DateTimeUtil
+
 data class NaverUserResponse(
     val response: NaverUserInnerResponse
 ) {
 
     fun toNaverUserProfile(): NaverUserProfile {
-        return NaverUserProfile(response.id, response.name)
+        println(response.birthYear)
+        println(response.birthDay)
+        println(response.profileImage)
+        return NaverUserProfile(
+            response.id,
+            response.name,
+            response.profileImage,
+            response.email,
+            response.gender,
+            DateTimeUtil.toLocalDate("${response.birthYear}-${response.birthDay}")
+        )
     }
 }
 
 data class NaverUserInnerResponse(
     val name: String,
-    val id: String
+    val id: String,
+    @JsonProperty("profile_image")
+    val profileImage: String,
+    val email: String,
+    val gender: String,
+    @JsonProperty("birthyear")
+    val birthYear: String,
+    @JsonProperty("birthday")
+    val birthDay: String
 )
