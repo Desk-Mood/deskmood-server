f81a57e26cdf4a46285135480670a92af32405b7
wwan13 <wwan13@naver.com>
2024-09-10 16:37:44 +0900

    chore : 스크립트 경로 수정

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -11,7 +11,7 @@ else
   domains=("deskmood.kr")
 fi
 rsa_key_size=4096
-data_path="../nginx/certbot"
+data_path="../.platform/nginx/certbot"
 email="wwan13@naver.com"
 staging=$2
 
@@ -34,7 +34,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker-compose -f ../docker-composoe."${1}".yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -43,11 +43,12 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose -f ../docker-composoe."${1}".yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
+docker-compose -f ../docker-composoe."${1}".yml up --force-recreate -d nginx
+ run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -70,7 +71,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker-compose -f ../docker-compose."$1".yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -81,4 +82,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-docker-compose -f ../docker-compose."$1".yml exec nginx nginx -s reload
