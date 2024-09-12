f3b88f37065ae941e98fea46726507b941ba9713
wwan13 <wwan13@naver.com>
2024-09-13 00:08:46 +0900

    test : oauth2 기본 프로필 조회 API api docs test

--- deskmood-core/core-api/src/test/kotlin/org/deskmood/docs/user/UserApiDocsTest.kt
+++ deskmood-core/core-api/src/test/kotlin/org/deskmood/docs/user/UserApiDocsTest.kt
@@ -8,6 +8,7 @@ import io.wwan13.api.document.snippets.ENUM
 import io.wwan13.api.document.snippets.NUMBER
 import io.wwan13.api.document.snippets.STRING
 import io.wwan13.api.document.snippets.isTypeOf
+import io.wwan13.api.document.snippets.moreAbout
 import io.wwan13.api.document.snippets.whichMeans
 import org.deskmood.controller.user.UserController
 import org.deskmood.controller.user.dto.UserAppendRequest
@@ -15,6 +16,7 @@ import org.deskmood.docs.ApiDocsTest
 import org.deskmood.domain.auth.OauthPlatform
 import org.deskmood.domain.user.Gender
 import org.deskmood.domain.user.UserService
+import org.deskmood.external.oauth.OauthPlatformSelector
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 
@@ -25,6 +27,9 @@ class UserApiDocsTest(
 
     @MockkBean
     private lateinit var userService: UserService
+
+    @MockkBean
+    private lateinit var oauthPlatformSelector: OauthPlatformSelector
 
     @Test
     fun `닉네임 중복 확인 API`() {
@@ -63,7 +68,7 @@ class UserApiDocsTest(
         }
 
         documentFor(api, "user-sign-up") {
-            summary("사용자 회원 가입 APi")
+            summary("사용자 회원 가입 API")
             requestFields(
                 "platform" isTypeOf ENUM(OauthPlatform::class) whichMeans "oauth2 플랫폼 (naver/google)",
                 "email" isTypeOf STRING whichMeans "이메일",
@@ -74,6 +79,35 @@ class UserApiDocsTest(
             )
             responseFields(
                 "data.id" isTypeOf NUMBER whichMeans "생성된 유저의 ID"
+            )
+        }
+    }
+
+    @Test
+    fun `oauth2 유저 기본 프로필 조회 API`() {
+        every { oauthPlatformSelector.select(any()) } returns StubOauth2UserProfileReader()
+
+        val api = api.get("/api/v1/user/oauth2/profile") {
+            queryParam("platform", "google")
+            queryParam("oauth2Code", "4/0AQlEd8wox65EfTuFeyHIaGMLV0lVo4tNAQiSW5Rh-bbrnbZKK9kZUzkPPfk5-yxdAxX2uQ&")
+            queryParam("redirectUri", "http://localhost:3000/auth")
+        }
+
+        documentFor(api, "oauth2-basic-user-profile") {
+            summary(
+                "Oauth2 플랫폼(구글/네이버) 에서 제공하는 기본 유저 정보를 조회하는 API"
+                    moreAbout "회원가입 전 기본 값을 불러오기 위해 호출하는 API 입니다."
+            )
+            queryParameters(
+                "platform" whichMeans "oauth2 플랫폼",
+                "oauth2Code" whichMeans "oauth2 로그인 요청 후 발급받은 code",
+                "redirectUri" whichMeans "등록된 redirect uri"
+            )
+            responseFields(
+                "data.email" isTypeOf STRING whichMeans "이메일",
+                "data.nickname" isTypeOf STRING whichMeans "닉네임",
+                "data.birth" isTypeOf STRING whichMeans "생일",
+                "data.gender" isTypeOf STRING whichMeans "성별 (남성/여성)",
             )
         }
     }
