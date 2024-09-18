82f45be7381d7e4e8d83e4ce6ed174cd796c19c6
wwan13 <wwan13@naver.com>
2024-09-18 13:07:06 +0900

    refactor : JpqlExecutor 적용

--- deskmood-storage/core-db/src/main/kotlin/org/deskmood/enity/user/UserJpaRepository.kt
+++ deskmood-storage/core-db/src/main/kotlin/org/deskmood/enity/user/UserJpaRepository.kt
@@ -1,6 +1,5 @@
 package org.deskmood.enity.user
 
-import com.linecorp.kotlinjdsl.support.spring.data.jpa.javax.repository.KotlinJdslJpqlExecutor
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserJpaRepository : JpaRepository<UserEntity, Long>, KotlinJdslJpqlExecutor
+interface UserJpaRepository : JpaRepository<UserEntity, Long>
