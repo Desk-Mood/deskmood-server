a384c0e1d1c6070db58a50a7ac67bc2d7ba9ff1f
wwan13 <wwan13@naver.com>
2024-09-19 13:12:47 +0900

    test : 닉네임 중복 확인 API 가이드 상세 작성

--- deskmood-core/core-api/src/test/kotlin/org/deskmood/docs/user/CheckNicknameAvailableApiDocsTest.kt
+++ deskmood-core/core-api/src/test/kotlin/org/deskmood/docs/user/CheckNicknameAvailableApiDocsTest.kt
@@ -18,6 +18,10 @@ class CheckNicknameAvailableApiDocsTest : UserApiDocsTest() {
 
         documentFor(api, "check-nickname-available") {
             summary("닉네임 중복 확인 APi")
+            guide(
+                "온보딩 과정에서 닉네임 중복을 확인하는 API",
+                "사용 가능한 닉네임인 경우 `true` / 중복되어 사용할 수 없는 닉네임인 경우 `false`룰 반환합니다."
+            )
             queryParameters(
                 "nickname" whichMeans "확인 하려는 닉네임"
             )
