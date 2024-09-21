54dbcc7e77c3b83ee16d1305bfbe73550b03a2e7
wwan13 <wwan13@naver.com>
2024-09-21 15:53:59 +0900

    feat : job 정렬 순서 재배치 기능 구현

--- deskmood-admin/admin-api/src/main/kotlin/org/deskmood/admin/domain/job/AdminJobRepository.kt
+++ deskmood-admin/admin-api/src/main/kotlin/org/deskmood/admin/domain/job/AdminJobRepository.kt
@@ -28,8 +28,69 @@ class AdminJobRepository(
         }
     }
 
+    fun findOrder(id: Long): Int? {
+        return jpqlExecutor.find {
+            select(
+                path(AdminJob::sortOrder)
+            ).from(
+                entity(AdminJob::class)
+            ).where(
+                path(AdminJob::id).eq(id)
+            )
+        }
+    }
+
     fun count(): Long {
         return jobJpaRepository.count()
+    }
+
+    @Transactional
+    fun modifyOrder(id: Long, to: Int): Int {
+        return jpqlExecutor.update {
+            update(
+                entity(AdminJob::class)
+            ).set(
+                path(AdminJob::sortOrder), to
+            ).whereAnd(
+                path(AdminJob::id).eq(id),
+            )
+        }
+    }
+
+    @Transactional
+    fun increaseSortOrderBetweenExcludeTarget(
+        target: Long,
+        start: Int,
+        end: Int
+    ) {
+        jpqlExecutor.update {
+            update(
+                entity(AdminJob::class)
+            ).set(
+                path(AdminJob::sortOrder), plus(path(AdminJob::sortOrder), 1)
+            ).whereAnd(
+                path(AdminJob::sortOrder).between(start, end),
+                path(AdminJob::id).notEqual(target)
+            )
+        }
+    }
+
+    @Transactional
+    fun decreaseSortOrderBetweenExcludeTarget(
+        target: Long,
+        start: Int,
+        end: Int
+    ) {
+        jpqlExecutor.update {
+            update(
+                entity(AdminJob::class)
+            ).set(
+                path(AdminJob::sortOrder), minus(path(AdminJob::sortOrder), 1)
+            ).whereAnd(
+                path(AdminJob::sortOrder).between(start, end),
+                path(AdminJob::id).notEqual(target)
+            )
+        }
     }
 
     @Transactional
--- deskmood-admin/admin-api/src/main/kotlin/org/deskmood/admin/error/AdminApiErrorType.kt
+++ deskmood-admin/admin-api/src/main/kotlin/org/deskmood/admin/error/AdminApiErrorType.kt
@@ -3,8 +3,35 @@ package org.deskmood.admin.error
 import org.deskmood.error.ErrorType
 import org.deskmood.error.LogLevel
 import org.deskmood.http.HttpStatus
+import kotlin.reflect.KClass
 
 interface AdminApiErrorType : ErrorType
+
+data class AdminNoSuchData<T : Any, R>(
+    val from: KClass<T>,
+    val id: R
+) : AdminApiErrorType {
+    override val httpStatusCode: HttpStatus = HttpStatus.BAD_REQUEST
+    override val errorCode: String = "ADMIN_API_400_1"
+    override val message: String = "찾으려는 데이터가 없습니다. (${from.simpleName}-$id)"
+    override val logLevel: LogLevel = LogLevel.INFO
+}
+
+data object AdminAccessFailed : AdminApiErrorType {
+    override val httpStatusCode: HttpStatus = HttpStatus.FORBIDDEN
+    override val errorCode: String = "ADMIN_API_403_0"
+    override val message: String = "접근 권한이 없습니다."
+    override val logLevel: LogLevel = LogLevel.INFO
+}
+
+data class AdminInvalidEnumValue(
+    val enumValues: List<String> = listOf()
+) : AdminApiErrorType {
+    override val httpStatusCode: HttpStatus = HttpStatus.BAD_REQUEST
+    override val errorCode: String = "ADMIN_API_400_2"
+    override val message: String = "유효하지 않은 enum value 입니다.[${enumValues.joinToString(",")}]"
+    override val logLevel: LogLevel = LogLevel.INFO
+}
 
 data object AdminAlreadyHasJobData : AdminApiErrorType {
     override val httpStatusCode: HttpStatus = HttpStatus.BAD_REQUEST
